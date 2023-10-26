8c9f6f07a6d732ef14b3736bb588712024c111a8
Josh Campbell <joshdcam@gmail.com>
2023-10-26 15:06:11 +1000

    update

--- winforge.ps1
+++ winforge.ps1
@@ -9,19 +9,11 @@ param (
 )
 
 # You can call your winforge.ps1 script with the parameters as follows:
-# . .\winforge.ps1 -theme 0 -background '#555555' -computerName "Bob's PC" -apps "www.list.com/myapplist.json"
+# . .\winforge.ps1 -theme 0 -background '#555555' -settings "www.list.com/settings.json" -computerName "Bob's PC" -apps "www.list.com/myapplist.json"
 
 
-# Example usage of the parameters
-Write-Host "Theme: $theme"
-Write-Host "Background: $background"
-Write-Host "Computer Name: $computerName"
-Write-Host "Apps: $apps"
-
-
-# SET REGISTRY ENTRIES FUNCTION
-# Example usage: Set-RegistryProperty -Path 'HKCU:\Software\Example' -Name 'SampleValue' -Value 'NewValue' -PropertyType 'String'
 function Set-RegistryProperty {
+# Example usage: Set-RegistryProperty -Path 'HKCU:\Software\Example' -Name 'SampleValue' -Value 'NewValue' -PropertyType 'String'
 
     param (
         [string]$Path,
@@ -34,79 +26,179 @@ function Set-RegistryProperty {
         # Registry entry exists, so set its value
         Set-ItemProperty -Path $Path -Name $Name -Value $Value -Type $PropertyType
         Write-Host "Registry entry '$Name' updated with value '$Value'."
-    } else {
+    }
+    else {
         # Registry entry doesn't exist, create it
         New-ItemProperty -Path $Path -Name $Name -Value $Value -PropertyType $PropertyType
         Write-Host "Registry entry '$Name' created with value '$Value'."
     }
 }
 
+function Import-RegistrySettingsFromFile {
+    if (-not $settings) {
+        $settings = Read-Host "Path to settings file URL (Enter nothing to skip)"
 
+        if (-not [string]::IsNullOrEmpty($settings)) {
 
-    # Function to install registry settings
-    function Install-RegistrySettings {
-        # Add your registry settings installation logic here
-        Write-Host "Installing registry settings..."
-    }
-    
-    # Function to set computer name
-    function Set-ComputerName {
-        # Add your computer name setting logic here
-        Write-Host "Setting computer name..."
-    }
-    
-    # Function to install applications
-    function Install-Apps {
-        # Add your application installation logic here
-        Write-Host "Installing applications..."
-    }
-    
-    # Function to set desktop background
-    function Set-DesktopBackground {
-        # Add your desktop background setting logic here
-        Write-Host "Setting desktop background..."
-    }
-    
-    # Function to set Windows taskbar colors
-    function Set-TaskbarColors {
-        # Add your taskbar color setting logic here
-        Write-Host "Setting Windows taskbar colors..."
+            # The user entered a path, so proceed with calling your script.
+            Get-Content $settings | ForEach-Object {
+                $line = $_.Trim()
+                if ($line -ne "" -and $line -match '^"([^"]+)" -Name "([^"]+)" -Value (.+)$') {
+                    $path = $matches[1]
+                    $name = $matches[2]
+                    $value = $matches[3]
+        
+                    if (-not (Test-Path $path)) {
+                        # Create the registry key (folder) if it doesn't exist
+                        New-Item -Path $path
+                    }
+        
+                    Set-ItemProperty -Path $path -Name $name -Value $value
+                    Write-Host "Updated: $path\$name"
+                }
+            }
+            Write-Host "Registry settings have been updated."
+            Pause 3
+            
+        }
+        else {
+            # The user entered nothing, so skip calling your script.
+            Write-Host "Script skipped."
+        }
 
     }
-    
-    # Function to deploy everything
-    function Deploy-Everything {
-        Install-RegistrySettings
-        Set-ComputerName
-        Install-Apps
-        Set-DesktopBackground
-        Set-TaskbarColors
-        Write-Host "Everything has been deployed."
-    }
-    
-    # Main menu loop
-    while ($true) {
-        Clear-Host
-        Show-SplashScreen
-        Write-Host "Main Menu"
-        Write-Host "1. Install Registry Settings"
-        Write-Host "2. Set Computer Name"
-        Write-Host "3. Install Applications"
-        Write-Host "4. Set Desktop Background"
-        Write-Host "5. Set Windows Taskbar Colors"
-        Write-Host "6. Deploy Everything"
-        Write-Host "0. Exit"
-    
-        $choice = Read-Host "Enter your choice:"
-    
-        switch ($choice) {
-            "1" { Install-RegistrySettings }
-            "2" { Set-ComputerName }
-            "3" { Install-Apps }
-            "4" { Set-DesktopBackground }
-            "5" { Set-TaskbarColors }
-            "6" { Deploy-Everything }
-            "0" { break }
-            Default { Write-Host "Invalid choice. Please try again." }
+}
+
+function Import-Settings {
+    # Example usage:
+    # Import-RegistrySettings -settings "https://raw.githubusercontent.com/graphixa/winforge/main/config.cfg"
+
+    param (
+        [string]$settings
+    )
+
+    if (-not [string]::IsNullOrEmpty($settings)) {
+        try {
+            # Use Invoke-RestMethod to fetch the file contents
+            $settingsContent = Invoke-RestMethod -Uri $settings
+
+            # Process the contents of the configuration file
+            $settingsContent | ForEach-Object {
+                $line = $_.Trim()
+                if ($line -ne "" -and $line -match '^"([^"]+)" -Name "([^"]+)" -Value (.+)$') {
+                    $path = $matches[1]
+                    $name = $matches[2]
+                    $value = $matches[3]
+
+                    if (-not (Test-Path $path)) {
+                        # Create the registry key (folder) if it doesn't exist
+                        New-Item -Path $path
+                    }
+
+                    Set-ItemProperty -Path $path -Name $name -Value $value
+                    Write-Host "Updated: $path\$name"
+                }
+            }
+            Write-Host "Registry settings have been updated."
+            Pause 3
+        }
+        catch {
+            Write-Host "An error occurred while fetching or processing the configuration file: $_"
         }
     }
+    else {
+        # The user entered nothing, so skip calling your script.
+        Write-Host "Script skipped."
+    }
+}
+
+
+
+
+function Set-Checkpoint {
+
+    do {
+        $choice = Read-Host "Would you like to create a system restore point? [Advisable]"
+    } while ($choice -ne "Y" -and $choice -ne "N")
+
+   if ($choice -eq "Y") {
+    Write-Host "Creating a system restore point..."
+  
+        try {
+        Enable-ComputerRestore -Drive "$env:systemdrive"
+        Checkpoint-Computer -Description "Winforge Customisation" -RestorePointType "MODIFY_SETTINGS" -Verbose
+        }
+    
+        catch {
+        Write-Host "Error:" $_.Exception.Message -ForegroundColor Red
+        Write-Host ""
+        Pause
+        Return
+        }
+    
+        Write-Host "System checkpoint created..."
+        Start-Sleep 1
+    }
+  
+  }
+
+# Function to set theme [ Light / Dark Mode ]
+function Set-Theme {
+    # Prompt for the theme preferencee if it's not provided as a parameter [1 - Use Light Theme | 0 - Use Dark Theme]
+    do {
+        $theme = Read-Host "Select your theme: 0 = Dark Theme | 1 = Light Theme"
+    } while ($theme -ne "0" -and $theme -ne "1")
+
+        Write-Host "Setting computer theme..."
+        Set-RegistryProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name SystemUsesLightTheme -Value $theme -PropertyType 'Dword'
+        Stop-Process -Name explorer -Force
+        Start-Process explorer
+}
+
+# Function to set computer name
+function Set-ComputerName {
+    if (-not $computerName) {
+        $computerName = Read-Host "Set your Computer Name:"
+
+    }
+    Write-Host "Setting computer name..."
+    
+    try {
+        Rename-Computer -NewName $computerName -Force
+    }
+    catch {
+        Write-Host "Error:" $_.Exception.Message -ForegroundColor Red
+    }
+    
+}
+    
+# Function to install applications
+function Install-Apps {
+    # Add your application installation logic here
+    Write-Host "Installing applications..."
+}
+    
+# Function to set desktop background
+function Set-DesktopBackground {
+    # Add your desktop background setting logic here
+    Write-Host "Setting desktop background..."
+}
+    
+# Function to set Windows taskbar colors
+function Set-TaskbarColors {
+    # Add your taskbar color setting logic here
+    Write-Host "Setting Windows taskbar colors..."
+
+}
+    
+# Function to deploy everything
+function DeployAll {
+    Set-Checkpoint
+    Set-Theme
+    Set-ComputerName
+    Import-RegistrySettings
+    Install-Apps
+    Set-DesktopBackground
+    Set-TaskbarColors
+    Write-Host "Everything has been deployed."
+}
