cf3f6e26c66e539570400ccf0cd486daa808b6ae
Josh Campbell <joshdcam@gmail.com>
2023-10-26 17:24:04 +1000

    Update

--- winforge.ps1
+++ winforge.ps1
@@ -2,18 +2,19 @@
 
 param (
     [int]$theme,
-    [string]$background,
-    [string]$settings,
     [string]$computerName,
+    [string]$wallpaper,
+    [string]$wallpaperStyle,
+    [string]$settings,
     [string]$apps
 )
 
 # You can call your winforge.ps1 script with the parameters as follows:
-# . .\winforge.ps1 -theme 0 -background '#555555' -settings "www.list.com/settings.json" -computerName "Bob's PC" -apps "www.list.com/myapplist.json"
+# . .\winforge.ps1 -theme 0 -wallpaper '#555555' -wallpaperStyle 'fill' -settings "www.list.com/settings.json" -computerName "Bob's PC" -apps "www.list.com/myapplist.json"
 
 
 function Set-RegistryProperty {
-# Example usage: Set-RegistryProperty -Path 'HKCU:\Software\Example' -Name 'SampleValue' -Value 'NewValue' -PropertyType 'String'
+    # Example usage: Set-RegistryProperty -Path 'HKCU:\Software\Example' -Name 'SampleValue' -Value 'NewValue' -PropertyType 'String'
 
     param (
         [string]$Path,
@@ -34,86 +35,7 @@ function Set-RegistryProperty {
     }
 }
 
-function Import-RegistrySettingsFromFile {
-    if (-not $settings) {
-        $settings = Read-Host "Path to settings file URL (Enter nothing to skip)"
-
-        if (-not [string]::IsNullOrEmpty($settings)) {
-
-            # The user entered a path, so proceed with calling your script.
-            Get-Content $settings | ForEach-Object {
-                $line = $_.Trim()
-                if ($line -ne "" -and $line -match '^"([^"]+)" -Name "([^"]+)" -Value (.+)$') {
-                    $path = $matches[1]
-                    $name = $matches[2]
-                    $value = $matches[3]
-        
-                    if (-not (Test-Path $path)) {
-                        # Create the registry key (folder) if it doesn't exist
-                        New-Item -Path $path
-                    }
-        
-                    Set-ItemProperty -Path $path -Name $name -Value $value
-                    Write-Host "Updated: $path\$name"
-                }
-            }
-            Write-Host "Registry settings have been updated."
-            Pause 3
-            
-        }
-        else {
-            # The user entered nothing, so skip calling your script.
-            Write-Host "Script skipped."
-        }
-
-    }
-}
-
-function Import-Settings {
-    # Example usage:
-    # Import-RegistrySettings -settings "https://raw.githubusercontent.com/graphixa/winforge/main/config.cfg"
-
-    param (
-        [string]$settings
-    )
-
-    if (-not [string]::IsNullOrEmpty($settings)) {
-        try {
-            # Use Invoke-RestMethod to fetch the file contents
-            $settingsContent = Invoke-RestMethod -Uri $settings
-
-            # Process the contents of the configuration file
-            $settingsContent | ForEach-Object {
-                $line = $_.Trim()
-                if ($line -ne "" -and $line -match '^"([^"]+)" -Name "([^"]+)" -Value (.+)$') {
-                    $path = $matches[1]
-                    $name = $matches[2]
-                    $value = $matches[3]
-
-                    if (-not (Test-Path $path)) {
-                        # Create the registry key (folder) if it doesn't exist
-                        New-Item -Path $path
-                    }
-
-                    Set-ItemProperty -Path $path -Name $name -Value $value
-                    Write-Host "Updated: $path\$name"
-                }
-            }
-            Write-Host "Registry settings have been updated."
-            Pause 3
-        }
-        catch {
-            Write-Host "An error occurred while fetching or processing the configuration file: $_"
-        }
-    }
-    else {
-        # The user entered nothing, so skip calling your script.
-        Write-Host "Script skipped."
-    }
-}
-
-
-
+# ----------------------------------
 
 function Set-Checkpoint {
 
@@ -121,84 +43,247 @@ function Set-Checkpoint {
         $choice = Read-Host "Would you like to create a system restore point? [Advisable]"
     } while ($choice -ne "Y" -and $choice -ne "N")
 
-   if ($choice -eq "Y") {
-    Write-Host "Creating a system restore point..."
+    if ($choice -eq "Y") {
+        Write-Host "Creating a system restore point..."
   
         try {
-        Enable-ComputerRestore -Drive "$env:systemdrive"
-        Checkpoint-Computer -Description "Winforge Customisation" -RestorePointType "MODIFY_SETTINGS" -Verbose
+            Enable-ComputerRestore -Drive "$env:systemdrive"
+            Checkpoint-Computer -Description "Winforge Customisation" -RestorePointType "MODIFY_SETTINGS" -Verbose
         }
     
         catch {
-        Write-Host "Error:" $_.Exception.Message -ForegroundColor Red
-        Write-Host ""
-        Pause
-        Return
+            Write-Host "Error:" $_.Exception.Message -ForegroundColor Red
+            Write-Host ""
+            Pause
+            Return
         }
     
         Write-Host "System checkpoint created..."
         Start-Sleep 1
     }
   
-  }
+}
 
-# Function to set theme [ Light / Dark Mode ]
+function Set-ComputerName {
+    if (-not $computerName) {
+        $computerName = Read-Host "Set your Computer Name:"
+
+    }
+    Write-Host "Setting computer name now..."
+    
+    try {
+        Rename-Computer -NewName $computerName -Force
+        Start-Sleep 1
+    }
+    catch {
+        Write-Host "Error:" $_.Exception.Message -ForegroundColor Red
+    }
+    Write-Host "Computer name set to: $computerName"
+    Start-Sleep 1
+    Clear-Host
+}
+
 function Set-Theme {
     # Prompt for the theme preferencee if it's not provided as a parameter [1 - Use Light Theme | 0 - Use Dark Theme]
     do {
         $theme = Read-Host "Select your theme: 0 = Dark Theme | 1 = Light Theme"
     } while ($theme -ne "0" -and $theme -ne "1")
 
-        Write-Host "Setting computer theme..."
-        Set-RegistryProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name SystemUsesLightTheme -Value $theme -PropertyType 'Dword'
-        Stop-Process -Name explorer -Force
-        Start-Process explorer
+    Write-Host "Setting computer theme..."
+    Set-RegistryProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name SystemUsesLightTheme -Value $theme -PropertyType 'Dword'
+    Stop-Process -Name explorer -Force
+    Start-Process explorer
 }
 
-# Function to set computer name
-function Set-ComputerName {
-    if (-not $computerName) {
-        $computerName = Read-Host "Set your Computer Name:"
-
-    }
-    Write-Host "Setting computer name..."
+Function Set-WallPaper {
+ 
+<#
+ 
+    .SYNOPSIS
+    Applies a specified wallpaper to the current user's desktop
     
+    .PARAMETER Image
+    Provide the exact path to the image
+ 
+    .PARAMETER Style
+    Provide wallpaper style (Example: Fill, Fit, Stretch, Tile, Center, or Span)
+  
+    .EXAMPLE
+    Set-WallPaper wallpaper "C:\Wallpaper\Default.jpg"
+    Set-WallPaper -Image "C:\Wallpaper\Background.jpg" -Style Fit
+  
+#>
+
+ 
+param (
+    [string]$wallpaper,
+    # Provide wallpaper style that you would like applied
+    [parameter(Mandatory=$False)]
+    [ValidateSet('Fill', 'Fit', 'Stretch', 'Tile', 'Center', 'Span')]
+    [string]$WallpaperStyle
+)
+
+if (-not $wallpaper) {
+    $wallpaper = Read-Host "Image URL for your desktop background?"
+    
+}
+if (-not [string]::IsNullOrEmpty($wallpaper)) {
+
     try {
-        Rename-Computer -NewName $computerName -Force
+        # Use Invoke-RestMethod to fetch the file contents
+        $wallpaperPath = Invoke-RestMethod -Uri $wallpaper -OutFile "$HOME\Pictures\wallpaper.jpg"
     }
     catch {
-        Write-Host "Error:" $_.Exception.Message -ForegroundColor Red
+        Write-Host "Error occured importing wallpaper. Check the URL provided"
     }
-    
+
+ 
+$Style = Switch ($WallpaperStyle) {
+  
+    "Fill" {"10"}
+    "Fit" {"6"}
+    "Stretch" {"2"}
+    "Tile" {"0"}
+    "Center" {"0"}
+    "Span" {"22"}
+  
+}
+ 
+If($Style -eq "Tile") {
+ 
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $style -Force
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 1 -Force
+ 
+}
+Else {
+ 
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $style -Force
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 0 -Force
+ 
+}
+ 
+Add-Type -TypeDefinition @" 
+using System; 
+using System.Runtime.InteropServices;
+  
+public class Params
+{ 
+    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
+    public static extern int SystemParametersInfo (Int32 uAction, 
+                                                   Int32 uParam, 
+                                                   String lpvParam, 
+                                                   Int32 fuWinIni);
+}
+"@ 
+  
+    $SPI_SETDESKWALLPAPER = 0x0014
+    $UpdateIniFile = 0x01
+    $SendChangeEvent = 0x02
+  
+    $fWinIni = $UpdateIniFile -bor $SendChangeEvent
+  
+    $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $wallpaper, $fWinIni)
+}
+else {
+    # The user entered nothing, so skip setting the wallpaper.
+    Write-Host "Wallpaper import skipped."
 }
     
-# Function to install applications
-function Install-Apps {
-    # Add your application installation logic here
-    Write-Host "Installing applications..."
 }
-    
-# Function to set desktop background
-function Set-DesktopBackground {
-    # Add your desktop background setting logic here
-    Write-Host "Setting desktop background..."
-}
-    
-# Function to set Windows taskbar colors
+
 function Set-TaskbarColors {
-    # Add your taskbar color setting logic here
+
     Write-Host "Setting Windows taskbar colors..."
 
 }
-    
-# Function to deploy everything
+
+
+function Install-Apps {
+
+    if (-not $wallpaper) {
+        $wallpaper = Read-Host "Image URL for your desktop background?"
+        
+    }
+    if (-not [string]::IsNullOrEmpty($wallpaper)) {
+
+
+# GitHub repository URL
+$apps = "https://raw.githubusercontent.com/graphixa/Winforge/main/applist.list"
+
+# Fetch the list of apps from GitHub
+$appsList = Invoke-RestMethod -Uri $githubRepoUrl
+
+# Create an array to hold the apps
+$wingetapps = @()
+
+# Split the lines in the list and add them to the array
+$appsList -split "`r`n" | ForEach-Object {
+    $wingetapps += $_
+}
+
+# Print the array for verification
+$wingetapps
+    Write-Host "Installing applications..."
+}
+}
+
+function Import-OOShutupSettings {
+    # Example usage:
+    # Import-RegistrySettings -settings "https://raw.githubusercontent.com/graphixa/winforge/main/config.cfg"
+
+    param (
+        [string]$settings
+    )
+    if (-not $settings) {
+        $settings = Read-Host "URL to your settings file:"
+    }
+        
+    if (-not [string]::IsNullOrEmpty($settings)) {
+        try {
+            
+            # Use Invoke-RestMethod to fetch the file contents
+            $settingsContent = Invoke-RestMethod -Uri $settings
+
+            # The user entered a path, so proceed with calling your script.
+            Get-Content $settingsContent | ForEach-Object {
+                $line = $_.Trim()
+                if ($line -ne "" -and $line -match '^"([^"]+)" -Name "([^"]+)" -Value (.+)$') {
+                    $path = $matches[1]
+                    $name = $matches[2]
+                    $value = $matches[3]
+
+                    if (-not (Test-Path $path)) {
+                        # Create the registry key (folder) if it doesn't exist
+                        New-Item -Path $path
+                    }
+
+                    Set-ItemProperty -Path $path -Name $name -Value $value
+                    Write-Host "Updated: $path\$name"
+                }
+            }
+            Write-Host "Registry settings have been updated."
+            Start-Sleep 2
+        }
+        catch {
+            Write-Host "An error occurred while fetching or processing the configuration file: $_"
+        }
+    }
+    else {
+        # The user entered nothing, so skip calling your script.
+        Write-Host "Settings import skipped."
+        Start-Sleep 1
+    }
+}
+
 function DeployAll {
     Set-Checkpoint
     Set-Theme
-    Set-ComputerName
-    Import-RegistrySettings
-    Install-Apps
-    Set-DesktopBackground
     Set-TaskbarColors
+    Set-ComputerName
+    Import-Settings
+    Install-Apps
+    Set-Desktopwallpaper
+    
     Write-Host "Everything has been deployed."
+    Pause
 }
